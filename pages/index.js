2b6b143f2afa420de43894aba4a52672fdb011f8
MaxwellNeves <maxwellgomes.94@hotmail.com>
2022-09-15 22:09:09 -0300

    update-inteface

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const index = () => {
+import {Product, FooterBanner, HeroBanner} from '../components'
+
+const Home = () => {
   return (
-    <div>index</div>
+    <>
+    HeroBanner
+
+    <div className='products-heading'>
+    <h2>Best Selling Products</h2>
+    <p>Speakers of many variations</p>
+    </div>
+
+    <div className='products-container'>
+      {['Product 1', 'Product 2'].map((product) => product)}
+    </div>
+
+    Footer
+    </>
   )
 }
 
-export default index
+export default Home;
